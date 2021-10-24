621d3d7c07766654cfa8b16b27472eb81d2b251e
gsalgado2138 <92330322+gsalgado2138@users.noreply.github.com>
2021-10-24 13:29:45 -0500

    Add files via upload

---
+++ class lab.py
@@ -0,0 +1,33 @@
+#program that prints hello world
+
+#giovanni
+
+#october 12,2021
+# 1
+print("hello world")
+#2 
+user= input('what is your name')
+
+greet= ('my name is')
+print(greet + " " + user)
+#3
+name1= input("name of proffesor:")
+print( "hello", greet, "and", name1)
+#4
+pi= 3.147
+print( " the area of a circle is" )
+print(pi) 
+#5
+miles= int(input('current miles: '))
+gallon= int(input('gallons used: '))
+print('have a good day drive safe')
+
+#6
+celsius= float(input('celsius: '))
+fahrenheit= (celsius ^ 9/5)+32
+fahrenheit= round( fahrenheit, 2)
+print( celsius, ' celsius is: ', fahrenheit, 'fahrenheit')
+
+
+
+
