59f74ecc8b424d717ceed789171650dabdc68276
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-30 18:00:45 -0500

    Update class lab.py

--- class lab.py
+++ class lab.py
@@ -5,26 +5,41 @@
 #october 12,2021
 # 1
 print("hello world")
+
 #2 
 user= input('what is your name')
-
 greet= ('my name is')
 print(greet + " " + user)
+
 #3
 name1= input("name of proffesor:")
 print( "hello", greet, "and", name1)
+
 #4
 pi= 3.147
+'''
+area of a circle is,
+area = pi* radius **2
+'''
 print( " the area of a circle is" )
-print(pi) 
+print(pi)  '''print(area)''' 
+
 #5
 miles= int(input('current miles: '))
 gallon= int(input('gallons used: '))
+'''
+You need to find MPG using miles and gallons.
+MPG = miles/gallon
+'''
 print('have a good day drive safe')
 
 #6
+''' 
+This question needs to convert Fahrenheight to Celsius.
+Fahrenheit = float(input("Fahrentheit"))
+'''
 celsius= float(input('celsius: '))
-fahrenheit= (celsius ^ 9/5)+32
+fahrenheit= (celsius ^ 9/5)+32   '''Incorrect Computation'''
 fahrenheit= round( fahrenheit, 2)
 print( celsius, ' celsius is: ', fahrenheit, 'fahrenheit')
 
